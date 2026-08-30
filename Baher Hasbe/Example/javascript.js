ef6b6d15a3ffe96abd062ab6992b18adfde0e367
mulusmy <228323835+mulusmy@users.noreply.github.com>
2026-08-30 20:18:56 +0300

    Update javascript.js

--- Baher Hasbe/Example/javascript.js
+++ Baher Hasbe/Example/javascript.js
@@ -1,1 +1,7 @@
+const axios = require('axios');
 
+axios.get('https://api.mulusmy.com/bh/v1/', {
+    params: { year: 2016, key: 'YOUR_API_KEY' }
+})
+.then(res => console.log(res.data))
+.catch(err => console.error(err));
